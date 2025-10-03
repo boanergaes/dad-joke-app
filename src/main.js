c80e9fd6ac76f2b9229d526c4cfaa6d778164709
boanergaes <mebitzeamanuel@gmail.com>
2025-10-03 12:09:23 +0300

    did the random jode functionality

--- src/main.js
+++ src/main.js
@@ -1,24 +1,56 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const jokeDisplay = document.getElementById('joke-display')
+const randomJokeBtn = document.getElementById('random-joke-btn')
+const saveJokeBtn = document.getElementById('save-joke-btn')
+const toolBarPopup = document.getElementById('tool-bar-popup')
 
-setupCounter(document.querySelector('#counter'))
+async function getRandomJoke() {
+  jokeDisplay.textContent = 'Fetching joke...'
+  try {
+    const randomJoke = await fetch('https://icanhazdadjoke.com/', {
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+    const data = await randomJoke.json()
+    jokeDisplay.textContent = data['joke']
+  }
+  catch(err) {
+      jokeDisplay.textContent = 'Error: There was trouble fetching the joke!'
+      console.log(err)
+  }
+}
+
+randomJokeBtn.addEventListener('click', getRandomJoke)
+
+function getJokeStorage() {
+  let jokes = localStorage.getItem('jokes')
+  if (!jokes) {
+    localStorage.setItem('jokes', JSON.stringify(new Set()))
+    jokes = localStorage.getItem('jokes')
+  }
+  console.log(JSON.parse(jokes))
+  return JSON.parse(jokes)
+}
+
+function storeJoke(joke) {
+  const jokes = getJokeStorage()
+  if (jokes.has(joke)) return false
+  jokes.add(joke)
+  localStorage.setItem('jokes', JSON.stringify(jokes))
+  return true
+}
+
+saveJokeBtn.addEventListener('click', () => {
+  const joke = jokeDisplay.textContent
+  if (joke == '') {
+    toolBarPopup.textContent = 'No joke to save!'
+  }
+  else if (storeJoke(jokeDisplay.textContent)) toolBarPopup.textContent = 'Joke saved!'
+  else toolBarPopup.textContent = 'This joke is already saved!'
+  toolBarPopup.style.display = 'block'
+  setTimeout(() => {
+    toolBarPopup.style.display = 'none'
+  }, 1200)
+})
